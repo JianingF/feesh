0dbe550403b3e4ed2a749bfc8ee64c2a9d28bc3f
JianingF <feng.amy1@gmail.com>
2025-04-13 16:50:47 -0400

    shadows

--- entities/characters/player/scripts/character_body_3d.gd
+++ entities/characters/player/scripts/character_body_3d.gd
@@ -24,17 +24,13 @@ func update_animation() -> void:
 func _ready() -> void:
 	anim_player.animation_changed.connect(set_spritesheet)
 
-const SPEED: float = 2.0
+const SPEED: float = 5.0
 const JUMP_VELOCITY: float = 4.5
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
